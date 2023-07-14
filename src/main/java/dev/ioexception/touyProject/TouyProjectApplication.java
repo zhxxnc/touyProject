fd268077ca08737a1ab846eb9b83dc693526a08b
zxhxxn <zxhxxn@naver.com>
2023-07-14 14:05:21 +0900

    init

---
+++ src/main/java/dev/ioexception/touyProject/TouyProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.ioexception.touyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ioexception/touyProject/TouyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ioexception.touyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
